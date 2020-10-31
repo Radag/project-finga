13e68ab5e73cf11210d7243403dfc0ccbd619501
Adam Navratil <radaqs@gmail.com>
2020-10-31 20:40:08 +0100

    error presenter

---
+++ app/presenters/ErrorPresenter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\Responses;
+use Nette\Application\IResponse;
+use Tracy\ILogger;
+
+
+class ErrorPresenter implements Nette\Application\IPresenter
+{
+    use Nette\SmartObject;
+    
+    /** @var ILogger */
+    private $logger;
+
+
+    public function __construct(ILogger $logger)
+    {
+            $this->logger = $logger;
+    }
+
+
+    public function run(Nette\Application\Request $request): IResponse
+    {
+            $exception = $request->getParameter('exception');
+
+            if ($exception instanceof Nette\Application\BadRequestException) {
+                    return new Responses\ForwardResponse($request->setPresenterName('Error4xx'));
+            }
+
+            $this->logger->log($exception, ILogger::EXCEPTION);
+            return new Responses\CallbackResponse(function () {
+                    require __DIR__ . '/templates/Error/500.phtml';
+            });
+    }
+
+}
