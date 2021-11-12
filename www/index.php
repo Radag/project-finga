a7c1569b0c058609caebdbd10ed8561e816ce742
Adam Navratil <radaqs@gmail.com>
2021-11-12 22:03:18 +0100

    new

--- www/index.php
+++ www/index.php
@@ -1,5 +1,73 @@
-<?php
+<html>
+    <head>
+        <title>Finga fingerboards</title>
+        <meta name="google-site-verification" content="QB5Id9l3DrpY1n4NeuOu9yT5aQVxFb-neiln-v40cGY" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="description" content="Finga fingerboards - firma zabývající se ruční výrobou dřevěných fingerboardových desek, která byla založena Petrem Ptáčkem v roce 2005.">
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon"/>
+        <style>
+            body {
+                text-align: center;
+                font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+            }
+            img {
+                width: 240px;
+                margin-top: 20px;
+            }
+            .button-wrapper {
+                margin-top: 40px;
+                display: flex;
+                align-items: flex-start;
+                justify-content: center;
+            }
 
+            .button-wrapper a {
+                text-decoration: none;
+                height: 60px;
+                width: 200px;
+                background-color: black;
+                color: white;
+                font-size: 20px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 7px;
+                font-weight: bold;
+            }
+            .button-wrapper p {
+                color: black;
+                font-size: 11px;
+                font-weight: bold;
+            }
+
+            .button-wrapper .button {
+                text-decoration: none;
+                margin: 10px;
+                width: 200px;
+                margin-left: 40px;
+                margin-right: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="/images/finga_bw_logo.png" alt="logo">
+        <div class="button-wrapper">
+            <div class="button">
+                <a href="https://www.fingafingerboards.cz/">ČESKY</a>
+                <p>PRO ČESKÉ A SLOVENSKÉ ZÁKAZNÍKY</p>
+            </div>
+            <div class="button">
+                <a href="https://fingafingerboards.bigcartel.com/">ENGLISH</a>
+                <p>FOR ALL CUSTOMERS OUT OF CZECH AND SLOVAK REPUBLIC</p>
+            </div>
+        </div>
+
+    </body>
+</html>
+
+
+<?php
+/*
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -11,3 +79,4 @@ App\Bootstrap::boot()
 	->createContainer()
 	->getByType(Nette\Application\Application::class)
 	->run();
+*/
